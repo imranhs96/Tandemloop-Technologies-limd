e2bf1681ba6c485704b24b072e8a2b21fcd2127f
imranhs96 <85359080+imranhs96@users.noreply.github.com>
2021-09-20 22:00:21 +0530

    Program-2.py

---
+++ Program-2.py
@@ -0,0 +1,12 @@
+n=int(input('Enter the value   :'))
+j=1
+if n%2 !=0:
+    for i in range(1,n+1):
+        print(j,end=' ')
+        j+=2
+   
+else:
+    for i in range(1,n+1):
+        print(j,end=' ')
+        j+=2
+    
